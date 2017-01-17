ebb0ef61d5276b6b3e1329a0869e6fcdb0d9504b
jha <jha@PCP-JHA2>
2017-01-18 08:33:12 +1300

    boilerplate test projects

---
+++ tests/Fundamental.Tests/SanityTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fundamental.Tests
+{
+    [TestFixture]
+    public class SanityTest
+    {
+        [Test]
+        public void AmISane()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
