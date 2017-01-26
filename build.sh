d653c61f59450e46d4271727b0cbe4d81db67b67
jha <jamesjharper@outlook.com>
2017-01-26 20:45:25 +1300

    Corrected typo in test

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 mkdir -p /usr/lib/mono/xbuild-frameworks/.NETFramework/v4.6
 dotnet restore
 dotnet build src/**/project.json
-dotnet tests tests/nFundamental.Interface.Wasapi.Tests
-dotnet tests tests/nFundamental.Tests
+dotnet test tests/nFundamental.Interface.Wasapi.Tests
+dotnet test tests/nFundamental.Tests
