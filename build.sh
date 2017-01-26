0ca00373bcea28e553789f26a22d3afe95750298
jha <jamesjharper@outlook.com>
2017-01-26 20:41:55 +1300

    Attempt to have unit tests run on travis

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p /usr/lib/mono/xbuild-frameworks/.NETFramework/v4.6
-dotnet restore && dotnet build src/**/project.json
+dotnet restore
+dotnet build src/**/project.json
+dotnet tests tests/nFundamental.Interface.Wasapi.Tests
+dotnet tests tests/nFundamental.Tests
