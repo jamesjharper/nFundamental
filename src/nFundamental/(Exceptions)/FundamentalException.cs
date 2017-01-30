7d73c21798a50d315cf51c1d6908cfccf276c66b
jha <jamesjharper@outlook.com>
2017-01-30 05:15:34 +0100

    Added base type exception

---
+++ src/nFundamental/(Exceptions)/FundamentalException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fundamental
+{
+ 
+    public class FundamentalException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FundamentalException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public FundamentalException(string message) : base(message)
+        {
+        }
+    }
+}
