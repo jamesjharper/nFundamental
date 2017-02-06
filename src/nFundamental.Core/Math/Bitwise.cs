a45200999113206e7b0d0829afe64886173e5f42
James Harper <jamesjharper@outlook.com>
2017-02-06 19:42:44 +0100

    Added bitwise operations and tests.

---
+++ src/nFundamental.Core/Math/Bitwise.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Fundamental.Core.Math
+{
+    public static class Bitwise
+    {
+
+        /// <summary>
+        /// Finds the numbers the of flagged bits using magic.
+        /// Thank you Stack overflow!
+        /// http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+        /// </summary>
+        /// <param name="i">The i.</param>
+        /// <returns></returns>
+        public static int NumberOfSetBits(uint i)
+        {
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            return (int)(((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+        }
+
+        /// <summary>
+        /// Finds the numbers the of flagged bits using magic.
+        /// Thank you Stack overflow!
+        /// http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+        /// </summary>
+        /// <param name="i">The i.</param>
+        /// <returns></returns>
+        public static int NumberOfSetBits(int i)
+        {
+            return NumberOfSetBits(unchecked ((uint)i));
+        }
+
+        /// <summary>
+        /// Determines whether [is square of2] [the specified i].
+        /// </summary>
+        /// <param name="i">The i.</param>
+        /// <returns>
+        ///   <c>true</c> if [is square of2] [the specified i]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsSquareOf2(int i)
+        {
+            if (i == 0)
+                return true;
+            return NumberOfSetBits(unchecked((uint)i)) == 1;
+        }
+
+        private static readonly uint[] MulDeBruijnBit = {
+                 0,  9,  1, 10, 13, 21,  2, 29, 11, 14, 16, 18, 22, 25,  3, 30,
+                 8, 12, 20, 28, 15, 17, 24,  7, 19, 27, 23,  6, 26,  5,  4, 31
+              };
+
+
+        /// <summary>
+        /// Logs x to the base of 2.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <returns></returns>
+        public static int LogBase2(int x)
+        {
+            return (int)LogBase2(unchecked((uint)x));
+        }
+
+        /// <summary>
+        /// Logs x to the base of 2.
+        /// </summary>
+        /// <param name="x">The v.</param>
+        /// <returns></returns>
+        public static uint LogBase2(uint x)
+        {
+            x |= x >> 1; x |= x >> 2; x |= x >> 4; x |= x >> 8; x |= x >> 16;
+            return MulDeBruijnBit[(x * 0x07C4ACDDu) >> 27];
+        }
+
+        /// <summary>
+        /// Base 2 power of the given exponent.
+        /// </summary>
+        /// <param name="exponent">The exponent.</param>
+        /// <returns></returns>
+        public static int PowerBase2(int exponent)
+        {
+            if(exponent > 30)
+                throw new ArgumentException("Power base 2 can not exceed exponent 30 of a signed integer.");
+            return 1 << exponent;
+        }
+
+        /// <summary>
+        /// Base 2 power of the given exponent.
+        /// </summary>
+        /// <param name="exponent">The exponent.</param>
+        /// <returns></returns>
+        public static uint PowerBase2(uint exponent)
+        {
+            if (exponent > 31)
+                throw new ArgumentException("Power base 2 can not exceed exponent 31 of a unsigned integer.");
+            return (uint)(1 << unchecked((int)exponent));
+        }
+
+
+        /// <summary>
+        /// Rounds to nearest base 2 power.
+        /// </summary>
+        /// <param name="base2">The base2.</param>
+        /// <returns></returns>
+        public static int RoundDownToNearestBase2Power(int base2)
+        {
+            return base2 == 0 ? 0 : PowerBase2(LogBase2(base2));
+        }
+
+        /// <summary>
+        /// Rounds to nearest base 2 power.
+        /// </summary>
+        /// <param name="base2">The base2.</param>
+        /// <returns></returns>
+        public static uint RoundDownToNearestBase2Power(uint base2)
+        {
+            return base2 == 0 ? 0 : PowerBase2(LogBase2(base2));
+        }
+    }
+}
---
+++ tests/nFundamental.Core.Tests/Math/BitwiseTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fundamental.Core.Math;
+using NUnit.Framework;
+
+namespace Fundamental.Core.Tests.Math
+{
+    [TestFixture]
+    public class BitwiseTests
+    {
+        [TestCase(/* Input */ 0,  /* Expected */  0)]
+        [TestCase(/* Input */ 1,  /* Expected */  1)]
+        [TestCase(/* Input */ 2,  /* Expected */  1)]
+        [TestCase(/* Input */ 3,  /* Expected */  2)]
+        [TestCase(/* Input */ 16, /* Expected */  1)]
+        [TestCase(/* Input */ -1, /* Expected */ 32)]
+        public void CanCountBits(int input, int expectedFlaggedBitsCount)
+        {
+            Assert.AreEqual(expectedFlaggedBitsCount, Bitwise.NumberOfSetBits(input));
+        }
+
+
+        [TestCase(/* Input */ 0,  /* Expected */  true)]
+        [TestCase(/* Input */ 1,  /* Expected */  true)]
+        [TestCase(/* Input */ 2,  /* Expected */  true)]
+        [TestCase(/* Input */ 3,  /* Expected */  false)]
+        [TestCase(/* Input */ 10, /* Expected */  false)]
+        [TestCase(/* Input */ 16, /* Expected */  true)]
+        public void KnowsIfIsSquareOf2(int input, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Bitwise.IsSquareOf2(input));
+        }
+
+
+
+        [TestCase(/* Input */ 0,  /* Expected */  0)]
+        [TestCase(/* Input */ 1,  /* Expected */  0)]
+        [TestCase(/* Input */ 2,  /* Expected */  1)]
+        [TestCase(/* Input */ 3,  /* Expected */  1)]
+        [TestCase(/* Input */ 4,  /* Expected */  2)]
+        [TestCase(/* Input */ 5,  /* Expected */  2)]
+        [TestCase(/* Input */ 16, /* Expected */  4)]
+        [TestCase(/* Input */ 96, /* Expected */  6)]
+        [TestCase(/* Input */ 128, /* Expected */  7)]
+        public void CanFindLog2(int input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Bitwise.LogBase2(input));
+        }
+
+        [TestCase(/* Input */ 0,  /* Expected */  1)]
+        [TestCase(/* Input */ 1,  /* Expected */  2)]
+        [TestCase(/* Input */ 2,  /* Expected */  4)]
+        [TestCase(/* Input */ 3,  /* Expected */  8)]
+        [TestCase(/* Input */ 4,  /* Expected */  16)]
+        [TestCase(/* Input */ 5,  /* Expected */  32)]
+        [TestCase(/* Input */ 6,  /* Expected */  64)]
+        [TestCase(/* Input */ 7,  /* Expected */  128)]
+        [TestCase(/* Input */ 30, /* Expected */  1073741824)]
+        public void CanFindPowerBase2(int input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Bitwise.PowerBase2(input));
+        }
+
+
+        [TestCase(/* Input */ 0,  /* Expected */  0)]
+        [TestCase(/* Input */ 1,  /* Expected */  1)]
+        [TestCase(/* Input */ 2,  /* Expected */  2)]
+        [TestCase(/* Input */ 3,  /* Expected */  2)]
+        [TestCase(/* Input */ 4,  /* Expected */  4)]
+        [TestCase(/* Input */ 5,  /* Expected */  4)]
+        [TestCase(/* Input */ 16, /* Expected */  16)]
+        [TestCase(/* Input */ 17, /* Expected */  16)]
+        [TestCase(/* Input */ 24, /* Expected */  16)]
+        [TestCase(/* Input */ 96, /* Expected */  64)]
+        [TestCase(/* Input */ 128, /* Expected */  128)]
+        public void CanRoundToNearestBase2Power(int input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Bitwise.RoundDownToNearestBase2Power(input));
+        }
+
+    }
+}
