584d775ee7b127e3b697cd46c5ff9b36db517b07
James Harper <jamesjharper@outlook.com>
2017-02-08 20:45:47 +0100

    Added Format not supported exception

---
+++ src/nFundamental.Core/(Exceptions)/FormatNotSupportedException.cs
@@ -0,0 +1,13 @@
+﻿namespace Fundamental.Core
+{
+    public class FormatNotSupportedException : FundamentalException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormatNotSupportedException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public FormatNotSupportedException(string message) : base(message)
+        {
+        }
+    }
+}
